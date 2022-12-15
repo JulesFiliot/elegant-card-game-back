72d10b9e63755cf99e275b86901f171e1a813ae6
Matthieu Gourdon <matthieu.gourdon@tpc26.cpe.lan>
2022-12-15 11:56:17 +0100

    post rout /notify added

--- notifier/service/notification.js
+++ notifier/service/notification.js
@@ -1,4 +1,10 @@
-exports.notify = (req,res,callback) =>{
+exports.notify = (req,res,callback) => {
     res.send(req.params.msg)
     return 200
+};
+
+exports.notif = (req,res, callback) => {
+    let user_name = req.body.user;
+    let pwd = req.body.password;
+    res.send("User name = "+user_name+", password is "+pwd);
 };
