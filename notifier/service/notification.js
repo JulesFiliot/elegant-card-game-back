85f9bcd8b6678bca84089ebfa02c5ea6e1298d1c
Matthieu Gourdon <matthieu.gourdon@tpc26.cpe.lan>
2022-12-15 10:55:53 +0100

    hot fix notifier

--- notifier/service/notification.js
+++ notifier/service/notification.js
@@ -1,41 +1,4 @@
-/*
-var https = require('https');
-var http = require('http');
-
-https.globalAgent.maxSockets = 100;
-http.globalAgent.maxSockets = 100;
-*/
-
-
-const axios = require("axios");
-const mainURL = 'http://localhost:3001/';
-
 exports.notify = (req,res,callback) =>{
-    let URL = mainURL + 'notify/' + req.params.msg;
-    axios.get(URL)
-    .then((response) => {
-        /*
-        console.log(response.data);
-        console.log(response.status);
-        console.log(response.statusText);
-        console.log(response.headers);
-        console.log(response.config);
-        */
-        console.log(req.params.msg)
-        return callback(null, response.data);
-    }).catch(function (error) {
-        if (error.response) {
-            // Request made and server responded
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        } else if (error.request) {
-            // The request was made but no response was received
-            console.log(error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-        }
-        return callback(error);
-    });
+    res.send(req.params.msg)
+    return 200
 };
