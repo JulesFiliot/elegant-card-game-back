73d0370a9e2ad3ee3f08490a1842f791fd0dd06b
JulesFiliot <jules.filiot@cpe.fr>
2022-12-26 21:20:16 +0100

    [ADD] socket config on notifier server

--- notifier/server.js
+++ notifier/server.js
@@ -1,39 +1,32 @@
 const cors = require('cors');
 const express = require('express')
-const io = require('socket.io');
+const { Server } = require("socket.io");
 const http = require('http');
 const routes = require('./api/route');
+
 const app = express();
 const port = 3001;
-
-//ce code de websocket est a tester avec le front
-
-// si fonctionnel, mettre en place un socket.emit avec les
-// les informations necessaires pour le front en JSON 
 const server = http.createServer(app);
-const socketIo = io(server, {
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+const io = new Server(server, {
     cors: {
       origin: "*",
     }
 });
 
-socketIo.on('connection', (socket) => {
-    console.log("Client connected");
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
 });
 
-socketIo.on('connection', (socket) => {
-    socket.emit('message', 'Hello from the server!');
-});
-//fin du code a tester avec le front
-  
-
-let bodyParser = require('body-parser');
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(routes);
-app.listen(port, function() {
-    console.log('Serveur ecoute sur le port: ' + port);
+server.listen(port, () => {
+    console.log(`Server listening on: ${port}`);
 });
