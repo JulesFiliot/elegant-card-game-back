71a42b97d84ea2af1e7f9b4bccd652178d06c84b
JulesFiliot <jules.filiot@cpe.fr>
2022-12-26 20:42:40 +0100

    [WIP] notifier socket

--- notifier/server.js
+++ notifier/server.js
@@ -1,18 +1,21 @@
 const cors = require('cors');
 const express = require('express')
+const io = require('socket.io');
+const http = require('http');
+const routes = require('./api/route');
 const app = express();
 const port = 3001;
-const routes = require('./api/route');
 
 //ce code de websocket est a tester avec le front
 
 // si fonctionnel, mettre en place un socket.emit avec les
 // les informations necessaires pour le front en JSON 
-const io = require('socket.io');
-const http = require('http');
-
-const server = http.createServer();
-const socketIo = io(server);
+const server = http.createServer(app);
+const socketIo = io(server, {
+    cors: {
+      origin: "*",
+    }
+});
 
 socketIo.on('connection', (socket) => {
     console.log("Client connected");
