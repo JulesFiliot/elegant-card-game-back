c94271a4f9fecbeb912c250755d1d5a567c2f6d7
Matthieu Gourdon <matthieu.gourdon@cpe.fr>
2022-12-26 15:33:47 +0100

    commit for merge

--- notifier/server.js
+++ notifier/server.js
@@ -3,6 +3,26 @@ const app = express();
 const port = 3001;
 const routes = require('./api/route');
 
+//ce code de websocket est a tester avec le front
+
+// si fonctionnel, mettre en place un socket.emit avec les
+// les informations necessaires pour le front en JSON 
+const io = require('socket.io');
+const http = require('http');
+
+const server = http.createServer();
+const socketIo = io(server);
+
+socketIo.on('connection', (socket) => {
+    console.log("Client connected");
+});
+
+socketIo.on('connection', (socket) => {
+    socket.emit('message', 'Hello from the server!');
+});
+//fin du code a tester avec le front
+  
+
 let bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
