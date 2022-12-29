61fabca39b5fe8c2254d95e5963d078fdba9ac0c
JulesFiliot <jules.filiot@cpe.fr>
2022-12-29 19:11:00 +0100

    code cleaning

--- demo/src/main/java/com/example/demo/msgemitter/comm/controller/LoggsController.java
+++ demo/src/main/java/com/example/demo/msgemitter/comm/controller/LoggsController.java
@@ -15,15 +15,6 @@ public class LoggsController {
 	public LoggsController(loggsService loggsService){
 		this.loggsService=loggsService;
 	}
-	@JmsListener(destination = "logger/chat", containerFactory = "connectionFactory")
-    public void chatLogger(String msgStr) {
-		Loggs log= new Loggs();
-		log.setContent(msgStr);
-		log.setType("[CHAT]");
-		loggsService.addLoggs(log);		
-        System.out.println("[CHAT] RECEIVED String MSG=["+msgStr+"]");
-
-    }
 	@JmsListener(destination = "user_update", containerFactory = "connectionFactory")
     public void userLogger(UserDTO msgStr) {
 		Loggs log= new Loggs();
